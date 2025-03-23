69d73ab0abca0d27806bca5140d5f2cdf571f6bd
Damian <damian.pyrcz@edu.uekat.pl>
2025-03-23 16:09:07 +0100

    gotowe

--- 6.py
+++ 6.py
@@ -3,7 +3,8 @@ import cv2
 image = cv2.imread("img/lebron.jpg")
 
 cv2.imshow("obraz", image)
+cv2.namedWindow("obraz", cv2.WINDOW_NORMAL)
 cv2.waitKey(0)
-cv2.resizeWindow("obraz", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("obraz", 1024, 576)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
