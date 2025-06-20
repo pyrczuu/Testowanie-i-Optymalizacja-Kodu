e4ce6944591bd74a560a9e6ac86d11c970bd9b83
pyrczuu <dydekpyrcz@gmail.com>
2025-06-20 21:44:19 +0200

    poprawione

--- 6.py
+++ 6.py
@@ -10,3 +10,8 @@ cv2.imshow("Oryginalny", image)
 cv2.imshow("RBG - zmieniona kolejnosc", reordered)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+image[:, :, 2] = 0
+cv2.imshow("Usunięty czerwony", image)
+cv2.waitKey()
+
+
